db58273275b252acfd5bd558ded7c421f420ab66
Robin <robindev2019@outlook.com>
2025-10-12 21:14:54 +0200

    Reverse order of university and life

--- linio.ts
+++ linio.ts
@@ -23,7 +23,7 @@ const { values, positionals } = parseArgs({
 const ROOT = positionals[2] || process.cwd();
 
 // Default in case the CLI argument is omitted.
-const LISTS = ["all", "life", "university", "projects", "maakotheek", "qdentity", "dupunkto"];
+const LISTS = ["all", "university", "life", "projects", "maakotheek", "qdentity", "dupunkto"];
 
 const TODOS = ['EVERY', 'TODO', 'BACKLOG', 'DONE', 'NVM'];
 const WISHES = ['WISH', 'BOUGHT', 'NVM'];
