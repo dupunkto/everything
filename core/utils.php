87e9852c2e1bcf62b85d88cb6e3e79e05f6e4284
Robin <robindev2019@outlook.com>
2026-04-26 14:36:07 +0200

    Fix typo

--- core/utils.php
+++ core/utils.php
@@ -5,7 +5,7 @@ function generate_humid($length = 5, $base = 36) {
   return substr(str_pad(strtoupper(base_convert(unpack('N', random_bytes(4))[1], 10, $base)), $length, '0', STR_PAD_LEFT), -$length);
 }
 
-function cast_datetime_uct($date, $time, $timezone = null) {
+function cast_datetime_utc($date, $time, $timezone = null) {
   $timezone = $timezone ?? getenv("TIMEZONE") ?: "Europe/Amsterdam";
   $datetime = new DateTime("$date $time", new DateTimeZone($timezone));
   return $datetime->setTimezone(new DateTimeZone("UTC"))->format('c');
