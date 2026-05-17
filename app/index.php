6e8c553d70077da6788029664cfb29c7f9a36b86
Robin <robindev2019@outlook.com>
2026-05-17 09:35:09 +0200

    Add empty index and settings

---
+++ app/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include __DIR__ . "/shell/head.php" ?>
+    <title>Everything</title>
+  </head>
+  <body>
+    <?php include __DIR__ . "/shell/menu.php" ?>
+    <main>
+      
+    </main>
+  </body>
+</html>
