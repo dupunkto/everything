d8b0ba7341dfb6538d5f3e06f038bb8dec0eb04d
Robin <robindev2019@outlook.com>
2026-05-17 09:34:51 +0200

    Submit forms with Command-Enter

--- app/shell/head.php
+++ app/shell/head.php
@@ -4,6 +4,7 @@
 
 <link rel="stylesheet" href="<?= CANONICAL ?>/css/main.css" />
 
-<script src="<?= CANONICAL ?>/vendor/xhtml.min.js"></script> 
-<script src="<?= CANONICAL ?>/vendor/instant-page.min.js" type="module"></script> 
-<script src="<?= CANONICAL ?>/vendor/localtime.min.js"></script> 
+<script src="<?= CANONICAL ?>/vendor/xhtml.min.js"></script>
+<script src="<?= CANONICAL ?>/vendor/instant-page.min.js" type="module"></script>
+<script src="<?= CANONICAL ?>/vendor/forms.min.js" type="module"></script>
+<script src="<?= CANONICAL ?>/vendor/localtime.min.js"></script>
