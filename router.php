787c0515eb02e92438ba3e0e2c3c22dca3af3716
Robin <robindev2019@outlook.com>
2026-07-14 23:29:06 +0200

    Add basic caching in the dev server

--- router.php
+++ router.php
@@ -33,6 +33,8 @@ function is_https() {
 // Helpers for sending responses
 
 define('FORBIDDEN_MIMES', ["application/x-httpd-php"]);
+define('CACHEABLE_EXTENSIONS', ['css', 'js']);
+define('STATIC_CACHE_MAX_AGE', 3600);
 
 function serve_file($path) {
   $mime_type = path_mime($path) ?? "text/html";
@@ -41,8 +43,28 @@ function serve_file($path) {
     serve_error(403);
   }
 
+  if(in_array(path_ext($path), CACHEABLE_EXTENSIONS)) {
+    serve_cached($path);
+  }
+
   header("Content-Type: {$mime_type}");
   include $path; exit;
+}
+
+function serve_cached($path) {
+  $modified = filemtime($path);
+  $etag = '"' . md5($path . ":" . $modified . ":" . filesize($path)) . '"';
+
+  header("Cache-Control: public, max-age=" . STATIC_CACHE_MAX_AGE);
+  header("Last-Modified: " . gmdate("D, d M Y H:i:s", $modified) . " GMT");
+  header("ETag: $etag");
+
+  $since = @$_SERVER['HTTP_IF_MODIFIED_SINCE'];
+
+  if(@$_SERVER['HTTP_IF_NONE_MATCH'] == $etag or ($since and strtotime($since) >= $modified)) {
+    http_response_code(304);
+    exit;
+  }
 }
 
 function serve_error($code) {
