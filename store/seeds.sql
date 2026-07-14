8364c6384d9a423f717eee99f55ec436f6dcfdea
Robin <robindev2019@outlook.com>
2026-07-14 22:17:05 +0200

    Remove seeds

--- store/seeds.sql
+++ store/seeds.sql
@@ -1,12 +1,3 @@
 INSERT INTO `config`
   (`property`, `value`) VALUES
   ('host', 'localhost:4000');
-
-INSERT INTO `calendars`
-  (`id`, `title`, `subtitle`, `color`) VALUES
-  ('IBKP1', 'VOCUS', 'Commissies', '#ff2a04'),
-  ('8M84P', 'Universiteit Utrecht', 'Afspraken', '#ffcd00'),
-  ('HOB68', 'me, myself & i', NULL, '#63D87C'),
-  ('Y4UF0', 'friends & fam', NULL, '#ED6E8E'),
-  ('ICA9L', 'Qdentity', NULL, '#0075c9'),
-  ('40IO0', 'Maakotheek', NULL, '#f9af05');
