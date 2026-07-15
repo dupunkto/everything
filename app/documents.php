12b9c04e3525ef03f8f95b1bb783416d1b9414a0
Robin <robindev2019@outlook.com>
2026-07-15 21:19:31 +0200

    Add blank pages for documents, habits and email

---
+++ app/documents.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include __DIR__ . "/shell/head.php" ?>
+    <title>Documents</title>
+  </head>
+  <body>
+    <?php include __DIR__ . "/shell/menu.php" ?>
+  </body>
+</html>
---
+++ app/mail.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include __DIR__ . "/shell/head.php" ?>
+    <title>Mail</title>
+  </head>
+  <body>
+    <?php include __DIR__ . "/shell/menu.php" ?>
+  </body>
+</html>
