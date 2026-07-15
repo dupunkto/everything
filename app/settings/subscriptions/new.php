b6663b60bbebe758cbefbb5a1cd7385977272dba
Robin <robindev2019@outlook.com>
2026-07-15 20:17:31 +0200

    Hide form after adding subscription

--- app/settings/subscriptions/new.php
+++ app/settings/subscriptions/new.php
@@ -1,5 +1,12 @@
 <?php
 
+  $form = function() { ?>
+    <form class="subscription-new-form" x-post="/settings/subscriptions/new" x-target="#subscription-new" x-replace="outerHTML" x-refresh="#subscriptions-listing">
+      <input name="url" type="url" placeholder="iCalendar URL" required autofocus>
+      <button type="submit">Find</button>
+    </form>
+  <?php };
+
   if(isset($_POST['url'])) {
     $url = cast_string($_POST['url']);
     $feed = \ical\fetch_feed($url)
@@ -10,11 +17,11 @@
     \store\create_subscription($title, null, $url, cast_color($feed['color'] ?? "#efefef"))
       or fail("Could not create new subscription.");
 
-    include __DIR__ . "/listing.php"; exit;
+    ?>
+    <section id="subscription-new" z-dismiss="escape" hidden>
+      <?php $form() ?>
+    </section>
+    <?php exit;
   }
 
-?>
-<form class="subscription-new-form" x-post="/settings/subscriptions/new" x-target="#subscriptions-listing">
-  <input name="url" type="url" placeholder="iCalendar URL" required autofocus>
-  <button type="submit">Find</button>
-</form>
+  $form();
