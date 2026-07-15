9c2d16deb26359ccc68efeb75ac30d9dc8daafb2
Robin <robindev2019@outlook.com>
2026-07-15 21:19:41 +0200

    Remove habits from sidebar

--- app/shell/menu.php
+++ app/shell/menu.php
@@ -17,7 +17,6 @@ $actions = $section ? path_join(__DIR__, "..", $section, "actions.php") : null;
     <li><a href="/notes"><i class="fa-regular fa-notebook"></i> <span>Notes</span></a></li>
     <li><a href="/documents"><i class="fa-regular fa-books"></i> <span>Documents</span></a></li>
     <li><a href="/wishlist"><i class="fa-regular fa-book-heart"></i> <span>Wishlist</span></a></li>
-    <li><a href="/habits"><i class="fa-regular fa-spiral"></i> <span>Habits</span></a></li>
     <li><a href="/contacts"><i class="fa-regular fa-address-book"></i> <span>Contacts</span></a></li>
     <li><a href="/addresses"><i class="fa-regular fa-location-arrow"></i> <span>Addresses</span></a></li>
     <li><a href="/settings"><i class="fa-regular fa-gear"></i> <span>Settings</span></a></li>
