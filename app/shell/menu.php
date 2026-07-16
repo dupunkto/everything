68efd3d53c1fda1bbbc9b2fec27bb2e58e8a38db
Robin <robindev2019@outlook.com>
2026-07-16 19:56:22 +0000

    Add number shortcuts for applications

--- app/shell/menu.php
+++ app/shell/menu.php
@@ -10,16 +10,16 @@ $actions = $section ? path_join(__DIR__, "..", $section, "actions.php") : null;
   </div>
 
   <ul>
-    <li><a href="/mail"><i class="fa-regular fa-inbox"></i> <span>Mail</span></a></li>
-    <li><a href="/calendar"><i class="fa-regular fa-calendar"></i> <span>Calendar</span></a></li>
-    <li><a href="/todo"><i class="fa-regular fa-box-check"></i> <span>ToDo</span></a></li>
-    <li><a href="/tracker"><i class="fa-regular fa-timer"></i> <span>Tracker</span></a></li>
-    <li><a href="/notes"><i class="fa-regular fa-notebook"></i> <span>Notes</span></a></li>
-    <li><a href="/bookmarks"><i class="fa-regular fa-bookmark"></i> <span>Bookmarks</span></a></li>
-    <li><a href="/wishlist"><i class="fa-regular fa-book-heart"></i> <span>Wishlist</span></a></li>
-    <li><a href="/contacts"><i class="fa-regular fa-address-book"></i> <span>Contacts</span></a></li>
-    <li><a href="/addresses"><i class="fa-regular fa-location-arrow"></i> <span>Addresses</span></a></li>
-    <li><a href="/settings"><i class="fa-regular fa-gear"></i> <span>Settings</span></a></li>
+    <li><a z-key="1" href="/mail"><i class="fa-regular fa-inbox"></i> <span>Mail</span></a></li>
+    <li><a z-key="2" href="/calendar"><i class="fa-regular fa-calendar"></i> <span>Calendar</span></a></li>
+    <li><a z-key="3" href="/todo"><i class="fa-regular fa-box-check"></i> <span>ToDo</span></a></li>
+    <li><a z-key="4" href="/tracker"><i class="fa-regular fa-timer"></i> <span>Tracker</span></a></li>
+    <li><a z-key="5" href="/notes"><i class="fa-regular fa-notebook"></i> <span>Notes</span></a></li>
+    <li><a z-key="6" href="/bookmarks"><i class="fa-regular fa-bookmark"></i> <span>Bookmarks</span></a></li>
+    <li><a z-key="7" href="/wishlist"><i class="fa-regular fa-book-heart"></i> <span>Wishlist</span></a></li>
+    <li><a z-key="8" href="/contacts"><i class="fa-regular fa-address-book"></i> <span>Contacts</span></a></li>
+    <li><a z-key="9" href="/addresses"><i class="fa-regular fa-location-arrow"></i> <span>Addresses</span></a></li>
+    <li><a z-key="0" href="/settings"><i class="fa-regular fa-gear"></i> <span>Settings</span></a></li>
   </ul>
 
   <button type="button" class="nav__toggle" title="Toggle sidebar">
