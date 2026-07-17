05efd8ae0b6b5e13ed63b6415dece7e2d6d5dd11
Robin <robindev2019@outlook.com>
2026-07-17 00:20:58 +0000

    Support linking to contact

--- app/contacts.php
+++ app/contacts.php
@@ -8,6 +8,12 @@
   </head>
   <body>
     <?php include __DIR__ . "/shell/menu.php" ?>
+    <?php
+      $url = null; $kind = @$_GET['kind'] ?: 'person';
+
+      if(@$_GET['edit']) $url = "/contacts/edit?kind=" . rawurlencode($kind) . "&id=" . rawurlencode($_GET['edit']);
+      elseif(@$_GET['view']) $url = "/contacts/detail?kind=" . rawurlencode($kind) . "&id=" . rawurlencode($_GET['view']);
+    ?>
     <main class="main main--wide contacts" z-nav=".contacts__search, .contact-item">
       <aside class="contacts__sidebar">
         <form id="contacts-controls" class="contacts__controls">
@@ -28,7 +34,7 @@
         <section id="contacts-list" x-get="/contacts/listing" x-data="#contacts-controls"></section>
       </aside>
 
-      <section id="contacts-panel" class="contacts__panel"></section>
+      <section id="contacts-panel" class="contacts__panel"<?php if($url): ?> x-get="<?= esc_attr($url) ?>"<?php endif ?>></section>
     </main>
   </body>
 </html>
