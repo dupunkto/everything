f9137ce514404f245547e9cfc05607e1e65b04f3
Robin <robindev2019@outlook.com>
2026-07-30 18:44:26 +0200

    Remove asymetry from timings in calendar view

--- app/calendar/week.php
+++ app/calendar/week.php
@@ -151,7 +151,7 @@ $sidebar_right = UI_SIDEBAR_POSITION == 'right';
         <?php foreach($timings[$date] ?? [] as $timing): ?>
           <div class="day__timing" data-id="<?= esc_attr($timing['id']) ?>" title="<?= esc_attr($timing['title']) ?>"
                style="--timing-top: <?= $timing['top'] ?>; --timing-height: <?= $timing['height'] ?>; --timing-color: <?= esc_attr($timing['color']) ?>">
-            <span class="day__timing-dot day__timing-dot--start"><i class="fa-solid fa-clock"></i></span>
+            <span class="day__timing-dot day__timing-dot--start"></span>
             <span class="day__timing-dot day__timing-dot--end"></span>
           </div>
         <?php endforeach ?>
