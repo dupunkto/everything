849e2962d65b7e51d04d75e14a7e6ddb9f09e411
Robin <robindev2019@outlook.com>
2026-07-30 10:11:50 +0200

    Inline content.php in error.php

--- app/error.php
+++ app/error.php
@@ -15,7 +15,14 @@
   </head>
   <body>
     <main class="main request-error request-error--page">
-      <?php include __DIR__ . "/error/content.php" ?>
+      <h1 class="request-error__title"><span class="request-error__status"><?= $status ?></span> <?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?></h1>
+      <p><?= htmlspecialchars($message, ENT_QUOTES, 'UTF-8') ?></p>
+      <?php if($developer): ?>
+        <details class="request-error__trace">
+          <summary><?= htmlspecialchars(get_class($error), ENT_QUOTES, 'UTF-8') ?> in <?= htmlspecialchars($error->getFile(), ENT_QUOTES, 'UTF-8') ?>:<?= $error->getLine() ?></summary>
+          <pre><?= htmlspecialchars($error->getTraceAsString(), ENT_QUOTES, 'UTF-8') ?></pre>
+        </details>
+      <?php endif ?>
     </main>
   </body>
 </html>
--- app/error/content.php
+++
@@ -1,8 +0,0 @@
-<h1 class="request-error__title"><span class="request-error__status"><?= $status ?></span> <?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?></h1>
-<p><?= htmlspecialchars($message, ENT_QUOTES, 'UTF-8') ?></p>
-<?php if($developer): ?>
-  <details class="request-error__trace">
-    <summary><?= htmlspecialchars(get_class($error), ENT_QUOTES, 'UTF-8') ?> in <?= htmlspecialchars($error->getFile(), ENT_QUOTES, 'UTF-8') ?>:<?= $error->getLine() ?></summary>
-    <pre><?= htmlspecialchars($error->getTraceAsString(), ENT_QUOTES, 'UTF-8') ?></pre>
-  </details>
-<?php endif ?>
