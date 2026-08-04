fb2af28d229c6574699efa86c6cbc9cc81847a88
Robin <robindev2019@outlook.com>
2026-08-05 00:59:06 +0200

    Fix typos

--- app/settings/developer/clear-http-log.php
+++ app/settings/developer/clear-http-log.php
@@ -1,6 +1,6 @@
 <?php
 
   \store\clear_http_logs();
-  \store\put_audit_log('http_logs', '*', "Truncated HTTP log.", 'user', operation: 'delete');
+  \store\put_audit_log('http_log', '*', "Truncated HTTP log.", 'user', operation: 'delete');
 
   stay_on_page();
--- app/settings/developer/clear-system-log.php
+++ app/settings/developer/clear-system-log.php
@@ -1,6 +1,6 @@
 <?php
 
   \store\clear_system_logs();
-  \store\put_audit_log('system_logs', '*', "Truncated system log.", 'user', operation: 'delete');
+  \store\put_audit_log('system_log', '*', "Truncated system log.", 'user', operation: 'delete');
 
   stay_on_page();
