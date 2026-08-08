6927c773f76332dfa82a3a8a2347860bb17cb4fc
Robin <robindev2019@outlook.com>
2026-08-08 10:58:18 +0200

    Fix wrong backlink

--- app/settings/connectors.php
+++ app/settings/connectors.php
@@ -9,7 +9,7 @@
     <?php include __DIR__ . "/../shell/menu.php" ?>
     <main class="main">
       <header class="page-header">
-        <?php $parent = "/settings/applications"; include __DIR__ . "/back.php" ?>
+        <?php $parent = "/settings/setup"; include __DIR__ . "/back.php" ?>
         <h2>Connectors</h2>
         <button x-post="/settings/connectors/new" x-target="#connectors-listing">Add connector</button>
       </header>
